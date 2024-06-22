5053cd65011edc546369c9085b8793c83e04005b
zetdou <zyziupoz@gmail.com>
2024-06-22 14:02:05 +0200

    Update vite.config.js
    Set correct base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-03-image-finder/",
+  base: "/goit-react-hw-04-image-finder/",
   build: {
     outDir: "./dist",
   },
